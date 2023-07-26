001fa85e966d1e02302d5eefaae6e3e88cf089ca
inth9198 <inth2474@naver.com>
2023-07-26 11:21:51 +0900

    [feat]백엔드 연결

--- src/component/UploadButton.tsx
+++ src/component/UploadButton.tsx
@@ -1,6 +1,7 @@
+import axios from "axios";
 import React from "react";
 
-export default function UploadButton() {
+export default function UploadButton({ setUploadedImage }: any) {
   const [btnClass, setBtnClass] = React.useState("default");
   const inputRef = React.useRef<HTMLInputElement>(null);
   const handleButtonClick = () => {
@@ -11,6 +12,33 @@ export default function UploadButton() {
     if (event.target.files) {
       const file = event.target.files[0];
       console.log(file);
+      var reader = new FileReader();
+
+      reader.onloadend = function () {
+        setUploadedImage(reader.result);
+      };
+
+      if (file) {
+        reader.readAsDataURL(file);
+      } else {
+        setUploadedImage(null);
+      }
+      const formData = new FormData();
+
+      formData.append("file", file);
+
+      axios
+        .post("http://127.0.0.1:8000/face", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
     }
   };
   return (
--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -8,6 +8,7 @@ import bgimage2 from "../images/bg+title (1).png";
 import bgimage3 from "../images/bg+title (2).png";
 import bgimage4 from "../images/bg+title (3).png";
 import { ReactComponent as BtnIcon } from "../images/button.svg";
+import crystalImage from "../images/crystal.png";
 import Header from "../component/Header";
 import Footer from "../component/Footer";
 import MoveAnimation from "../component/MoveAnimation";
@@ -19,6 +20,7 @@ export default function MainPage() {
   const [currentIndex, setCurrentIndex] = React.useState(0);
   const [isLanding, setIsLanding] = React.useState(true);
   const [btnClass, setBtnClass] = React.useState("default");
+  const [uploadedImage, setUploadedImage] = React.useState("");
 
   const imageVariants = {
     hidden: { y: "100%" },
@@ -29,13 +31,55 @@ export default function MainPage() {
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
+  const MidComment = ({ isResult }: { isResult: boolean }) => {
+    if (isResult) {
+      return (
+        <div className="result-content">
+          <h1 className="result-wrapper">당신에 이미지에</h1>
+          <h1 className="result-num">78% </h1>
+          <h1 className="HYBE">
+            <span className="result-wrapper">
+              적합한 소속사는
+              <br />
+            </span>
+            <span className="result-span">HYBE</span>
+            <span className="result-wrapper"> 입니다. </span>
+          </h1>
+        </div>
+      );
+    }
+    return (
+      <div className="content">
+        <h1 className="crys-text-wrapper">
+          당신에 이미지에 <br />
+          가장 적합한 <br />
+          소속사를 찾아드립니다.
+        </h1>
+        <div className="crys-text-wrapper-bottom">
+          지금 사진 한장으로 확인해보세요!
+        </div>
+      </div>
+    );
+  };
   return (
     <div className="main">
       <Header />
       <div className="ai-body">
         <div className="flex-box-center">
           <div className="background">
-            <div className="background-image"></div>
+            <img
+              src={crystalImage}
+              className="background-image"
+              alt="background"
+            />
+            {uploadedImage && (
+              <img
+                src={uploadedImage}
+                className="foreground-image"
+                alt="foreground"
+              />
+            )}
+
             {isLanding ? (
               <Button
                 onClick={() => {
@@ -46,16 +90,7 @@ export default function MainPage() {
                 <BtnIcon />
               </Button>
             ) : (
-              <div className="content">
-                <h1 className="crys-text-wrapper">
-                  당신에 이미지에 <br />
-                  가장 적합한 <br />
-                  소속사를 찾아드립니다.
-                </h1>
-                <div className="crys-text-wrapper-bottom">
-                  지금 사진 한장으로 확인해보세요!
-                </div>
-              </div>
+              <MidComment isResult={!!uploadedImage} />
             )}
           </div>
           {isLanding ? (
@@ -90,7 +125,7 @@ export default function MainPage() {
             </>
           ) : (
             <div>
-              <UploadButton />
+              <UploadButton setUploadedImage={setUploadedImage} />
               <div className="text-wrapper-1">Development reference</div>
               <div className="text-wrapper-2">JoCoding Youtube</div>
               <div className="text-wrapper-2">Teachable Machine</div>
