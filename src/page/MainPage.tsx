8617690130cc9b02b199bb5b9c8bb2c0dbfa5c4a
TigerKim9 <90361096+TigerKim9@users.noreply.github.com>
2023-08-06 14:16:54 +0900

    Update MainPage.tsx

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -68,7 +68,7 @@ export default function MainPage() {
           <div className="result-content">
             <h1 className="result-wrapper">
               {errorMessage === "01"
-                ? "얼굴이 여러개가 보여요. 하나의 사진만 판단할 수 있어요!"
+                ? "얼굴이 여러개가 보여요."
                 : "얼굴이 보이지 않아요"}
             </h1>
             <h1 className="HYBE">
